11759642d63b1163514c97c9de50a083a60834f4
maki <maki@p200300ec371ca93e094eafa477b1ac00.dip0.t-ipconnect.de>
2024-11-17 15:23:24 +0100

    Improved keybindings with Nvim Oil

---
+++ lua/themes/onedark_improved.lua
@@ -0,0 +1,5 @@
+local better_maker = require 'better_theme_maker.maker'
+local theme = require 'themes.onedark'
+better_theme = better_maker(theme)
+-- better_theme = require('base46').override_theme(better_theme, 'catppuccin')
+return better_theme
