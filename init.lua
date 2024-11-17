a72eff5a006c772890f7635c95c9a869448f8fec
maki <maki@p200300ec371ca905a313453b3bbed87e.dip0.t-ipconnect.de>
2024-11-17 06:19:53 +0100

    Multicursor tweaks

--- init.lua
+++ init.lua
@@ -164,6 +164,7 @@ vim.opt.scrolloff = 10
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<C-c>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
