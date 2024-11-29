24332178f34049d2ef0d63827ba17b9fc24b9ed8
maki <maki@p200300ec371ca99e130081d95601d079.dip0.t-ipconnect.de>
2024-11-29 03:07:48 +0100

    Cleanup

--- lua/configs/vimtex.lua
+++
@@ -1,9 +0,0 @@
-vim.g.vimtex_quickfix_open_on_warning = 0
-vim.g.vimtex_compiler_latexmk = {
-  options = {
-    '-verbose',
-    '-file-line-error',
-    '-synctex=1',
-    '-interaction=nonstopmode',
-  },
-}
